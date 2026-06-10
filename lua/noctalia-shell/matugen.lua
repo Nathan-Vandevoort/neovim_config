219e78e391561b74558043b3be159588a5331eee
nathan <nathan.v.2021@gmail.com>
2026-06-09 17:50:05 -0700

    update

---
+++ lua/noctalia-shell/matugen.lua
@@ -0,0 +1,37 @@
+local M = {}
+
+function M.setup()
+    require('base16-colorscheme').setup {
+        -- Background tones
+        base00 = '#03233a',                -- Default Background
+        base01 = '#0f3857',      -- Lighter Background (status bars)
+        base02 = '#0b3350', -- Selection Background
+        base03 = '#5e6e73',                -- Comments, Invisibles
+        -- Foreground tones
+        base04 = '#afb4b6',     -- Dark Foreground (status bars)
+        base05 = '#f2f3f3',             -- Default Foreground
+        base06 = '#f2f3f3',             -- Light Foreground
+        base07 = '#f2f3f3',          -- Lightest Foreground
+        -- Accent colors
+        base08 = '#fd4663',                  -- Variables, XML Tags, Errors
+        base09 = '#6833ff',               -- Integers, Constants
+        base0A = '#3361ff',              -- Classes, Search Background
+        base0B = '#4dd1ff',                -- Strings, Diff Inserted
+        base0C = '#a180ff',     -- Regex, Escape Chars
+        base0D = '#80deff',      -- Functions, Methods
+        base0E = '#809cff',    -- Keywords, Storage
+        base0F = '#900017',        -- Deprecated, Embedded Tags
+    }
+end
+
+-- Register a signal handler for SIGUSR1 (matugen updates)
+local signal = vim.uv.new_signal()
+signal:start(
+    'sigusr1',
+    vim.schedule_wrap(function()
+        package.loaded['noctalia-shell.matugen'] = nil
+        require('noctalia-shell.matugen').setup()
+    end)
+)
+
+return M
--- lua/plugins/colorScheme.lua
+++ lua/plugins/colorScheme.lua
@@ -1,9 +1,20 @@
-return {
-    { 
-        "catppuccin/nvim", 
-        config = function()
-            vim.cmd.colorscheme("catppuccin-mocha")
-        end,
-    },
-
-}
+local has_noctalia = vim.fn.isdirectory(vim.fn.expand("~/.config/noctalia")) == 1
+if has_noctalia then
+    return {
+        {
+            "RRethy/base16-nvim",
+            config = function()
+                require("noctalia-shell.matugen").setup()
+            end,
+        },
+    }
+else
+    return {
+        {
+            "catppuccin/nvim",
+            config = function()
+                vim.cmd.colorscheme("catppuccin-mocha")
+            end,
+        },
+    }
+end
