818fafc36831fff386a74d9d8910bd563d427470
nathan <nathan.v.2021@gmail.com>
2026-07-05 17:22:05 -0700

    color config from noctalia shell

--- lua/noctalia-shell/matugen.lua
+++ lua/noctalia-shell/matugen.lua
@@ -3,23 +3,23 @@ local M = {}
 function M.setup()
     require('base16-colorscheme').setup {
         -- Background tones
-        base00 = '#03233a',                -- Default Background
-        base01 = '#0f3857',      -- Lighter Background (status bars)
-        base02 = '#0b3350', -- Selection Background
-        base03 = '#5e6e73',                -- Comments, Invisibles
+        base00 = '#0e142f',                -- Default Background
+        base01 = '#18214e',      -- Lighter Background (status bars)
+        base02 = '#161e46', -- Selection Background
+        base03 = '#5f6273',                -- Comments, Invisibles
         -- Foreground tones
-        base04 = '#afb4b6',     -- Dark Foreground (status bars)
-        base05 = '#f2f3f3',             -- Default Foreground
-        base06 = '#f2f3f3',             -- Light Foreground
-        base07 = '#f2f3f3',          -- Lightest Foreground
+        base04 = '#afb0b6',     -- Dark Foreground (status bars)
+        base05 = '#f2f2f3',             -- Default Foreground
+        base06 = '#f2f2f3',             -- Light Foreground
+        base07 = '#f2f2f3',          -- Lightest Foreground
         -- Accent colors
         base08 = '#fd4663',                  -- Variables, XML Tags, Errors
-        base09 = '#6833ff',               -- Integers, Constants
-        base0A = '#3361ff',              -- Classes, Search Background
-        base0B = '#4dd1ff',                -- Strings, Diff Inserted
-        base0C = '#a180ff',     -- Regex, Escape Chars
-        base0D = '#80deff',      -- Functions, Methods
-        base0E = '#809cff',    -- Keywords, Storage
+        base09 = '#bc63cf',               -- Integers, Constants
+        base0A = '#825cd6',              -- Classes, Search Background
+        base0B = '#677de4',                -- Strings, Diff Inserted
+        base0C = '#da96e9',     -- Regex, Escape Chars
+        base0D = '#93a2ec',      -- Functions, Methods
+        base0E = '#b096e9',    -- Keywords, Storage
         base0F = '#900017',        -- Deprecated, Embedded Tags
     }
 end
